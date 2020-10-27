dbd1c34d3fc77ff3d50341961adf6ac6161b438f
Greg <gregsino@gmail.com>
2020-10-27 15:15:45 +0100

    app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'studentlist-front';
+  title = 'Students list realized with Spring boot CRUD + Angular';
 }
